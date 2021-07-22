351851ce94eeba3c838b443f6d7e50a1ec282184
Ilya Gruzdev <ily.gruzdev99@gmail.com>
2021-07-22 10:16:01 +0700

    complete
    added images of app, all app functionality

--- contacts-uikit-app/ViewController.swift
+++ contacts-uikit-app/ViewController.swift
@@ -5,15 +5,78 @@
 //  Created by Илья Груздев on 22.07.2021.
 //
 
+import CoreTelephony
+import Contacts
+import ContactsUI
 import UIKit
 
-class ViewController: UIViewController {
+
+struct Person {
+    let name: String
+    let id: String
+    let number: String
+    let source: CNContact
+}
+
+class ViewController: UIViewController, UITableViewDataSource ,CNContactPickerDelegate, UITableViewDelegate{
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+        
+    }()
+    
+    var models = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(table)
+        table.frame = view.bounds
+        table.dataSource = self
+        table.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(didTapAdd))
     }
 
+    @objc func didTapAdd() {
+        let vc = CNContactPickerViewController()
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        let number = (contact.phoneNumbers[0].value ).value(forKey: "digits") as! String
+        let name = contact.familyName + " " + contact.givenName + " " + contact.middleName + " / " + number
+        let identifier = contact.identifier
+        let model = Person(name: name,
+                           id: identifier,
+                           number: number,
+                           source: contact
+        )
+        models.append(model)
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let url = URL(string: "tel://\(models[indexPath.row].number)") else {
+            return
+        }
+        UIApplication.shared.open(url)
+    }
 
 }
 
