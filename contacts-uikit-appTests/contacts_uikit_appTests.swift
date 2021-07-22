5665da68a8ed160a691c931427d96f87a42041df
Ilya Gruzdev <ily.gruzdev99@gmail.com>
2021-07-22 08:57:49 +0700

    Initial Commit

---
+++ contacts-uikit-appTests/contacts_uikit_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  contacts_uikit_appTests.swift
+//  contacts-uikit-appTests
+//
+//  Created by Илья Груздев on 22.07.2021.
+//
+
+import XCTest
+@testable import contacts_uikit_app
+
+class contacts_uikit_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
